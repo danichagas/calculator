577c6440aaf413e230ec974abe220f15240d8d1d
danichagas <chagasdaniel788@gmail.com>
2025-04-08 23:47:25 -0300

    feat: operations functions

--- calculator/Operations.cs
+++ calculator/Operations.cs
@@ -1,10 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace calculator;
-internal class Operations
+﻿namespace calculator;
+public class Operations
 {
+    public int sum(int number1, int number2)
+    {
+        return number1 + number2;
+    }
+
+    public int subtraction(int number1, int number2)
+    {
+        return number1 - number2;
+    }
+
+    public int multiplication(int number1, int number2)
+    {
+        return number1 * number2;
+    }
+
+    public double division(double number1, double number2)
+    {
+        string message = "Digite valores diferentes de 0";
+        if (number1 == 0 && number2 == 0)
+        {
+            Console.WriteLine(message);
+        }
+
+        return number2 / number1;
+    }
+
+    public double media(double number1, double number2)
+    {
+        return (number1 + number2) / 2;
+    }
 }
